ad97295df8cb999c626e0cfba86c7932b7036631
Chia-Wei Li <dreamcwli@gmail.com>
2012-12-15 13:30:50 +0800

    Support OTA updates with OTAUpdater

--- common.mk
+++ common.mk
@@ -2,9 +2,16 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/my/overlay/common
 
 PRODUCT_PACKAGES += \
+    OTAUpdater \
     Superuser \
     backuptool.functions \
     backuptool.sh \
     su
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    otaupdater.otaid=$(TARGET_PRODUCT)-jb-mr1 \
+    otaupdater.otaver=$(shell date +%Y%m%d) \
+    otaupdater.otatime=$(shell date +%Y%m%d-%H%M) \
+    otaupdater.sdcard.recovery=sdcard/0
 
 TARGET_OTA_BACKUP_ADDONS := true
